7733dcb98a670422af13801788d80f71d2487337
EsavingEU <marco.dematte89@gmail.com>
2026-07-08 12:08:26 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCWgT_T8hPvP3_S80FDxlo2NyE3OMFseaM",
+    authDomain: "azienda-agricola-cristina.firebaseapp.com",
+    projectId: "azienda-agricola-cristina",
+    storageBucket: "azienda-agricola-cristina.firebasestorage.app",
+    messagingSenderId: "394996803820",
+    appId: "1:394996803820:web:396983941c9cdae849ddc9"
 };
 
 // Initialize Firebase
